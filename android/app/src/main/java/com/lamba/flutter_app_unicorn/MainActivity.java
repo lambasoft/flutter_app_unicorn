de824e6f0afae3dba975a0a73f790976f647db29
lambasoft <lambasoftwares@gmail.com>
2019-03-22 17:36:12 +0200

    first commit

---
+++ android/app/src/main/java/com/lamba/flutter_app_unicorn/MainActivity.java
@@ -0,0 +1,13 @@
+package com.lamba.flutter_app_unicorn;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
